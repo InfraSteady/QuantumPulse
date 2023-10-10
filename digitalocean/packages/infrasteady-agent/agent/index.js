51ab0a9d2beb83334833514afef1aac69aac3e59
sghosh1810 <sghosh1810@gmail.com>
2023-10-11 02:47:37 +0530

    Use gots inbuilt searchParams instead of manually creating final endpoint.

--- digitalocean/packages/infrasteady-agent/agent/index.js
+++ digitalocean/packages/infrasteady-agent/agent/index.js
@@ -2,21 +2,15 @@ import got from "got";
 //event consists of query params
 export async function main(event, context) {
     const { requestTestId, requestEndpoint, requestType, requestHeaders, requestBody, requestQueryParams, requestTimeout } = event;
-    const finalRequestEndpoint = getFinalEndPoint(requestEndpoint, requestQueryParams);
-    const endpointResponse = await got(finalRequestEndpoint, {
+    const endpointResponse = await got(requestEndpoint, {
         timeout: { request: requestTimeout || 8000 },
         method: requestType,
         headers: requestHeaders,
+        searchParams: requestQueryParams,
     });
 
     const { timings, statusCode, headers } = endpointResponse;
-    const responseData = headers["content-type"] && headers["content-type"].includes("application/json") ? JSON.parse(endpointResponse.body) : endpointResponse.body;
+    const responseData = endpointResponse.body;
 
     return { body: { requestTestId: requestTestId, timings: timings, clientResponse: { data: responseData, headers: headers, statusCode: statusCode } } };
-}
-
-function getFinalEndPoint(requestEndpoint, requestQueryParams) {
-    return `${requestEndpoint}${Object.keys(requestQueryParams || {}).length ? "?" : ""}${Object.keys(requestQueryParams || {})
-        .map((key) => `${key}=${requestQueryParams[key]}`)
-        .join("&")}`;
 }
