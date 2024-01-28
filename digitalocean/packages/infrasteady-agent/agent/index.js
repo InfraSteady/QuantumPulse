73682951620798caf98d8f3e284418bc6e480755
sghosh1810 <sghosh1810@gmail.com>
2024-01-28 23:53:26 +0530

    Refactor with zed test

--- digitalocean/packages/infrasteady-agent/agent/index.js
+++ digitalocean/packages/infrasteady-agent/agent/index.js
@@ -1,16 +1,34 @@
 import got from "got";
 //event consists of query params
 export async function main(event, context) {
-    const { requestTestId, requestEndpoint, requestType, requestHeaders, requestBody, requestQueryParams, requestTimeout } = event;
-    const endpointResponse = await got(requestEndpoint, {
-        timeout: { request: requestTimeout || 8000 },
-        method: requestType,
-        headers: requestHeaders,
-        searchParams: requestQueryParams,
-    });
+  const {
+    requestTestId,
+    requestEndpoint,
+    requestType,
+    requestHeaders,
+    requestBody,
+    requestQueryParams,
+    requestTimeout,
+  } = event;
+  const endpointResponse = await got(requestEndpoint, {
+    timeout: { request: requestTimeout || 8000 },
+    method: requestType,
+    headers: requestHeaders,
+    searchParams: requestQueryParams,
+  });
 
-    const { timings, statusCode, headers } = endpointResponse;
-    const responseData = endpointResponse.body;
+  const { timings, statusCode, headers } = endpointResponse;
+  const responseData = endpointResponse.body;
 
-    return { body: { requestTestId: requestTestId, timings: timings, clientResponse: { data: responseData, headers: headers, statusCode: statusCode } } };
+  return {
+    body: {
+      requestTestId: requestTestId,
+      timings: timings,
+      clientResponse: {
+        data: responseData,
+        headers: headers,
+        statusCode: statusCode,
+      },
+    },
+  };
 }
